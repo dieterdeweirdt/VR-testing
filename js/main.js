e182e6d63ffa05e33742bde4565009dad8f6668e
Dieter De Weirdt <dieter.deweirdt@arteveldehs.be>
2024-03-14 14:05:10 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { ARButton } from 'three/addons/webxr/ARButton.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 // Scène opzetten
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.4, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -13,47 +13,56 @@ renderer.xr.enabled = true; // WebXR inschakelen
 document.body.appendChild(renderer.domElement);
 
 //VR button
-document.body.appendChild( ARButton.createButton( renderer ) );
+document.body.appendChild( VRButton.createButton( renderer ) );
 
+// Create a directional light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+// Position the light behind the camera
+light.position.set(0, 0, 1);
+// Add the light to the scene
+scene.add(light);
 
-const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 3 );
-light.position.set( 0.5, 1, 0.25 );
-scene.add( light );
-
+const bridge = new THREE.CubeTextureLoader().setPath('https://threejs.org/examples/textures/cube/Bridge2/').load(['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg']);
+bridge.encoding = THREE.sRGBEncoding;
+scene.background = bridge;
 
 // OrbitControls toevoegen
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true; // Optioneel, voor een soepelere ervaring
 controls.dampingFactor = 0.25;
-controls.enableZoom = true;
+controls.enableZoom = false;
 
 // Kubus
 var cubeGeometry = new THREE.BoxGeometry();
-var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cubeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
-cube.position.x = -2; // Positie aanpassen
+cube.position.set(-2, -5, 0); // Positie aanpassen
 
 // Bal (Sfeer)
 var sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.z = -3; // Positie aanpassen
+sphere.position.set(0, -5, -5); // Positie aanpassen
 scene.add(sphere);
 
 // Kegel
 var coneGeometry = new THREE.ConeGeometry(0.5, 1, 32);
-var coneMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+var coneMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
 var cone = new THREE.Mesh(coneGeometry, coneMaterial);
 cone.position.x = 3; // Positie aanpassen
 scene.add(cone);
 
 // Camera
-camera.position.set(0, 0, 1);
+camera.position.set(0, 0, 10);
 
 // Renderlus
 var animate = function () {
     requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
 
     controls.update(); // Alleen nodig als controls.enableDamping of controls.autoRotate is ingeschakeld
 
